cac378d51697927d92633bceb0c440f54852d332
Brandy Mello <brandyamello@gmail.com>
2019-09-12 13:40:11 -0600

    Restart project with scss

---
+++ src/containers/FilmsDisplay/FilmsDisplay.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import images from '../../images/images.js';
+import Film from '../Film/Film';
+
+const FilmsDisplay = ({ films }) => {
+  console.log(images)
+  const mappedFilms = films.map((film, index) => {
+    return <Film
+      key={film.id}
+      title={film.title}
+      description={film.description}
+      photo={images[`film${index}`][1]}
+    />
+  })
+
+  return (
+
+    <div>
+      {mappedFilms}
+    </div>
+  )
+}
+
+export default FilmsDisplay;
